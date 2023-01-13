e020ad6930e7a0ff2c3d2575db6fb80b20b037c1
wangmengwei <1730313139@qq.com>
2023-01-13 14:55:19 +0800

    修改菜单展示样式

--- src/components/view/message/Menu.vue
+++ src/components/view/message/Menu.vue
@@ -7,46 +7,28 @@
         border
         default-expand-all
         :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-        <el-table-column
-            prop="date"
-            label="日期"
-            sortable
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="姓名"
-            sortable
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="address"
-            label="地址">
-        </el-table-column>
-        </el-table>
-
-        <el-table
-        :data="tableData1"
-        style="width: 100%"
-        row-key="id"
-        border
-        lazy
-        :load="load"
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-        <el-table-column
-            prop="date"
-            label="日期"
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="姓名"
-            width="180">
-        </el-table-column>
-        <el-table-column
-            prop="address"
-            label="地址">
-        </el-table-column>
+            <el-table-column
+                prop="date"
+                label="日期"
+                sortable>
+            </el-table-column>
+            <el-table-column
+                prop="name"
+                label="姓名"
+                sortable>
+            </el-table-column>
+            <el-table-column
+                prop="address"
+                label="地址">
+            </el-table-column>
+            <el-table-column
+                fixed="right"
+                label="操作">
+                <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                    <el-button type="text" size="small">编辑</el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </div>
 </template>
@@ -54,80 +36,61 @@
     export default {
         data() {
             return {
-            tableData: [{
-                id: 1,
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                id: 2,
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                id: 3,
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄',
-                children: [{
-                    id: 31,
-                    date: '2016-05-01',
+                tableData: [{
+                    id: 1,
+                    date: '2016-05-02',
                     name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
+                    address: '上海市普陀区金沙江路 1518 弄'
                 }, {
-                    id: 32,
+                    id: 2,
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    id: 3,
                     date: '2016-05-01',
                     name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }]
-            }, {
-                id: 4,
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }],
-            tableData1: [{
-                id: 1,
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                id: 2,
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                id: 3,
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄',
-                hasChildren: true
-            }, {
-                id: 4,
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }]
+                    address: '上海市普陀区金沙江路 1519 弄',
+                    children: [{
+                        id: 31,
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄'
+                    }, {
+                        id: 32,
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄'
+                    }]
+                }, {
+                    id: 4,
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }],
             }
         },
         methods: {
             load(tree, treeNode, resolve) {
-            setTimeout(() => {
-                resolve([
-                {
-                    id: 31,
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    id: 32,
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }
-                ])
-            }, 1000)
+                setTimeout(() => {
+                    resolve([
+                    {
+                        id: 31,
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄'
+                    }, {
+                        id: 32,
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄'
+                    }
+                    ])
+                }, 1000)
+            },
+            handleClick(row) {
+                console.log(row);
             }
-        },
+        }
     }
 </script>
