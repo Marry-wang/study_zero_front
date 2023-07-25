dc01fddfbf29320845ba0636ccf94f3a4bdebed7
wangmengwei <1730313139@qq.com>
2023-07-25 14:27:28 +0800

    fix:角色权限添加

--- src/view/role/index.vue
+++ src/view/role/index.vue
@@ -46,17 +46,19 @@
         :data="menuList"
         show-checkbox
         default-expand-all
+        ref="tree"
         :default-checked-keys="defaultMenu"
         node-key="id"
         :props="defaultProps">
       </el-tree>
+      <el-button @click="saveRoleMenus" type="text" size="small">保存</el-button>
     </el-dialog>
   </div>
 </template>
 
 <script>
 import roleForm from './role-form.vue'
-import {getRoleList,delRole,getMenuList,getRoleMenuList} from '@/api/login/system'
+import {getRoleList,delRole,getMenuList,getRoleMenuList,addOrUpdateRole} from '@/api/login/system'
   export default {
     components:{roleForm},
     data() {
@@ -70,7 +72,9 @@ import {getRoleList,delRole,getMenuList,getRoleMenuList} from '@/api/login/syste
           id:'id',
           label:'menuName',
           children:'children'
-        }
+        },
+        selectRoleId:"",
+        selectMenus:[]
       }
     },
     mounted(){
@@ -100,16 +104,26 @@ import {getRoleList,delRole,getMenuList,getRoleMenuList} from '@/api/login/syste
         .catch(error=>console.log(error))
       },
       selectMenuList(row){
-        const roleId = row.roleId;
+        this.selectRoleId = row.roleId;
         const that =this;
         getMenuList({}).then(response=>{
           that.menuList = response.data;
           that.dialogFormVisible = true;
         }).catch(error=>console.log(error))
 
-        getRoleMenuList({"roleId":roleId}).then(response=>{
+        getRoleMenuList({"roleId":this.selectRoleId}).then(response=>{
           that.defaultMenu = response.data
         }).catch(error=>console.log(error))
+      },
+      saveRoleMenus(){
+        this.selectMenus =this.$refs.tree.getCheckedKeys();
+        const that =this;
+        addOrUpdateRole({"id":this.selectRoleId,"roleMenuIds":this.selectMenus}).then(function (response) {
+              that.dialogFormVisible = false;
+            })
+            .catch(function (error) {
+                console.log(error);
+        });
       }
     },
   }
