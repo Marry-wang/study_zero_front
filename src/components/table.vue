31e4ae3dff5800d53e1b0ce896c488afc3876dd7
wangmengwei <1730313139@qq.com>
2024-09-06 16:44:47 +0800

    fix:table样式调整

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,35 +1,39 @@
 <template>
     <div>
-        <el-button 
-            type="primary" 
-            icon="el-icon-plus"
-            @click="tableAdd()"
-        >
-            添加
-        </el-button>
-        <el-table :data="myTableData" style="width: 100%">
-            <el-table-column v-for="(item,index) in columnArr" :key="index" :prop="item.prop" :label="item.label" :width="item.width">
-            </el-table-column>
-            <el-table-column
-                fixed="right"
-                label="操作">
-                <template slot-scope="scope">
-                    <el-button  :disabled="scope.row.status==='1'" @click="tableUpdate(scope.row)" type="text" size="small">更新</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <div>
-            <el-pagination
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
-                :current-page="currentPage"
-                :page-sizes="[10, 50, 100]"
-                :page-size=pageSize
-                layout="sizes, prev, pager, next"
-                :total=pageTotal>
-            </el-pagination>
+        <div style="float: right;padding-right: 10px;">
+            <el-button style="position: relative;top: 10px; "
+                type="primary" 
+                icon="el-icon-plus"
+                @click="tableAdd()"
+            >
+                添加
+            </el-button>
         </div>
-        
+        <div style="position: relative;top: 10px; ">
+            <el-table :data="myTableData" style="width: 100%">
+                <el-table-column v-for="(item,index) in columnArr" :key="index" :prop="item.prop" :label="item.label" :width="item.width">
+                </el-table-column>
+                <el-table-column
+                    fixed="right"
+                    label="操作">
+                    <template slot-scope="scope">
+                        <el-button  :disabled="scope.row.status==='1'" @click="tableUpdate(scope.row)" type="text" size="small">更新</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div>
+                <el-pagination
+                    style="float: right;padding-right: 10px;"
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-sizes="[10, 50, 100]"
+                    :page-size=pageSize
+                    layout="sizes, prev, pager, next"
+                    :total=pageTotal>
+                </el-pagination>
+            </div>
+        </div>
     </div>
 </template>
 <script>
